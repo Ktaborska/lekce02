3b6fceed3e5b91581b7fd5c9adb379b3e7bd6afc
Kateřina Táborská <ktaborska92@gmail.com>
2022-02-15 22:24:45 +0100

    code editing

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 console.log ('Ahoj')
 
-// let element = document.querySelector('.text')
 
 function setBold (className, weight){
     console.log("Tady budu zvětšovat");
-    //element.style.fontWeight="bold"
     document.querySelector (className).style.fontWeight=weight;
 }
 
 function setNormal (className, weight){
     console.log("Tady budu zmenšovat");
-    //element.style.fontWeight="normal"
     document.querySelector (className).style.fontWeight=weight;
 }
 
